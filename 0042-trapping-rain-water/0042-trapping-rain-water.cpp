329f6ca457ea1fd981cbe9b75aeedb7ac9f7394a
pushpank-singh1 <pushpank1108@gmail.com>
2026-09-19 11:34:37 +0530

    Time: 0 ms (100%), Space: 26 MB (55.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,10 +3,10 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         int l = 0, r = n-1;
-        int lmax = 0 , rmax = 0;
+        int lmax = 0, rmax = 0;
         int ans = 0;
 
-        while(l < r) {
+        while(l < r){
             lmax = max(lmax, height[l]);
             rmax = max(rmax, height[r]);
 
